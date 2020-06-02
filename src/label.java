b6b84b2581519b344233a6be12a56e35e6809a3f
Anish Parasrampuria <anishparasram@gmail.com>
2020-06-02 15:12:40 -0700

    basic form created

--- src/label.java
+++ src/label.java
@@ -1,10 +1,21 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
 public class label {
 
 	private JFrame frame;
+	private JTextField txtName;
+	private JTextField txtEmail;
+	private JTextField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -34,8 +45,41 @@ public class label {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.ORANGE);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login Form");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(154, 23, 100, 31);
+		frame.getContentPane().add(lblNewLabel);
+		
+		txtName = new JTextField();
+		txtName.setText("Name");
+		txtName.setBounds(58, 73, 96, 19);
+		frame.getContentPane().add(txtName);
+		txtName.setColumns(10);
+		
+		txtEmail = new JTextField();
+		txtEmail.setText("Email");
+		txtEmail.setBounds(58, 112, 96, 19);
+		frame.getContentPane().add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		txtPassword = new JTextField();
+		txtPassword.setText("password");
+		txtPassword.setBounds(190, 73, 96, 19);
+		frame.getContentPane().add(txtPassword);
+		txtPassword.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null,txtEmail.getText( )); 
+			}
+		});
+		btnNewButton.setBounds(154, 218, 85, 21);
+		frame.getContentPane().add(btnNewButton);
 	}
-
 }
